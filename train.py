369cbc8e12bbdc803f16253e4151d263256978f7
aqweteddy <keddy940199@gmail.com>
2020-01-11 03:13:22 +0800

    train.py parser

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 
 import os
+import parser
 
 import torch
 import torch.nn as nn
@@ -13,12 +14,30 @@ from utils.dataloader import get_loader
 from torchvision import transforms
 
 from tqdm import tqdm
+from argparse import ArgumentParser
 
+parser = ArgumentParser()
+parser.add_argument('--model_path', type=str, default='models/' , help='path for saving trained models')
+parser.add_argument('--crop_size', type=int, default=224 , help='size for randomly cropping images')
+parser.add_argument('--vocab_path', type=str, default='data/vocab.txt', help='path for vocabulary wrapper')
+parser.add_argument('--image_dir', type=str, default='data/train2014_resize', help='directory for resized images')
+parser.add_argument('--caption_path', type=str, default='data/annotations/captions_train2014.json', help='path for train annotation json file')
+parser.add_argument('--save_ckpt', type=int , default=1000, help='step size for saving trained models(check points)')
 
+# Model parameters
+parser.add_argument('--embed_size', type=int , default=256, help='dimension of word embedding vectors')
+parser.add_argument('--hidden_size', type=int , default=512, help='dimension of lstm hidden states')
+parser.add_argument('--num_layers', type=int , default=1, help='number of layers in lstm')
+
+parser.add_argument('--num_epochs', type=int, default=5)
+parser.add_argument('--batch_size', type=int, default=128)
+parser.add_argument('--num_workers', type=int, default=4)
+parser.add_argument('--learning_rate', type=float, default=0.001)
+args = parser.parse_args()
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'CUDA: {torch.cuda.is_available()}')
 
-crop_size = 224
+crop_size = args.crop_size()
 transform = transforms.Compose([ 
 transforms.RandomCrop(crop_size),
 transforms.RandomHorizontalFlip(), 
@@ -26,16 +45,16 @@ transforms.ToTensor(),
 transforms.Normalize((0.485, 0.456, 0.406), 
                         (0.229, 0.224, 0.225))])
 dct = Dictionary()
-dct.load_dict('data/vocab.txt')
-data_loader = get_loader('data/train2014_resize', 'data/annotations/captions_train2014.json', dct, 
-                             transform, batch_size=128,
-                             shuffle=True, num_workers=4) 
+dct.load_dict(args.vocab_path)
+data_loader = get_loader(args.image_dir, args.caption_path, dct, 
+                             transform, batch_size=args.batch_size,
+                             shuffle=True, num_workers=args.num_workers) 
 
 
 
-def train(dct_size, num_layers=1, embed_size=256, hidden_size=512, epochs=10, save_step=1000, lr=0.001, model_save='model/'):
-    encoder = Encoder(embed_size=256).to(device)
-    decoder = Decoder(embed_size=256, hidden_size=512, dct_size=len(dct), num_layers=1).to(device)
+def train(dct_size, num_layers=1, embed_size=256, hidden_size=512, epochs=10, num_layers=1, save_step=1000, lr=0.001, model_save='model/'):
+    encoder = Encoder(embed_size=embed_size).to(device)
+    decoder = Decoder(embed_size=embed_size, hidden_size=hidden_size, dct_size=len(dct), num_layers=num_layer).to(device)
     criterion = nn.CrossEntropyLoss()
     params = list(decoder.parameters()) + list(encoder.linear.parameters()) + list(encoder.bn.parameters())
     optimizer = torch.optim.Adam(params, lr=lr)
@@ -61,5 +80,11 @@ def train(dct_size, num_layers=1, embed_size=256, hidden_size=512, epochs=10, sa
                     model_save, 'decoder-{}-{}.ckpt'.format(epoch+1, i+1)))
                 torch.save(encoder.state_dict(), os.path.join(
                     model_save, 'encoder-{}-{}.ckpt'.format(epoch+1, i+1)))
-train(len(dct), num_layers=1, embed_size=256, hidden_size=512, epochs=5, lr=0.001, save_step=1000, model_save='model/')
+train(len(dct), num_layers=args.num_layers, 
+                embed_size=args.embed_size, 
+                hidden_size=args.hidden_size, 
+                epochs=args.num_epochs, 
+                lr=args.learning_rate, 
+                save_step=args.save_ckpt, 
+                model_save=args.model_path)
 
---
+++ train.sh
@@ -0,0 +1,12 @@
+python train.py\
+        --model_path models\
+        --vocab_path data/vocab.txt\
+        --image_dir data/train2014_resize\
+        --caption_path data/annotations/captions_train2014.json\
+        --save_ckpt 2000\
+        --embed_size 256\
+        --hidden_size 512\
+        --num_layers 2\
+        --num_epochs 5\
+        --batch_size 64\
+        --learning_rate 0.001
